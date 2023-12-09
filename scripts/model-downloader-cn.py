2125f8e18d115da90265a071971c4dcebaa8c146
Edith Forsaken <138196095+EdithForsaken@users.noreply.github.com>
2023-12-09 15:40:48 +0700

    Update model-downloader-cn.py

--- scripts/model-downloader-cn.py
+++ scripts/model-downloader-cn.py
@@ -57,15 +57,15 @@ def request_civitai_detail(url):
     pattern = r'https://civitai\.com/models/(.+)'
     m = re.match(pattern, url)
     if not m:
-        return False, "不是一个有效的 civitai 模型页面链接，暂不支持"
+        return False, "Jika Bukan tautan yang valid ke halaman model civitai, tidak didukung saat ini"
 
     req_url = API_URL + "civitai/models/" + m.group(1)
     res = requests.get(req_url)
 
     if res.status_code >= 500:
-        return False, "呃 服务好像挂了，理论上我应该在修了，可以进群看看进度……"
+        return False, "Nah, layanan tampaknya sedang down, jadi secara teori saya seharusnya sedang mengusahakannya, jadi periksa dan lihat bagaimana perkembangannya.……"
     if res.status_code >= 400:
-        return False, "不是一个有效的 civitai 模型页面链接，暂不支持"
+        return False, "Jika Bukan tautan yang valid ke halaman model civitai, tidak didukung untuk saat ini!"
 
     if res.ok:
         return True, res.json()
@@ -103,20 +103,20 @@ def preview(url):
     more_guides = ""
     if resp["version"]["file"]["downloadUrl"]:
         has_download_file = True
-        more_guides = f'，点击下载按钮\n{resp["version"]["file"]["name"]}'
+        more_guides = f'，Klik pada tombol unduh\n{resp["version"]["file"]["name"]}'
 
 
-    return [f"预览成功{more_guides}"] + resp_to_components(resp) + \
+    return [f"Pratinjau Berhasil{more_guides}"] + resp_to_components(resp) + \
             [gr.update(interactive=has_download_file)]
 
 
 def download(model_type, filename, url, image_arr):
     if not (model_type and url and filename):
-        return "下载信息缺失"
+        return "Informasi unduhan yang hilang"
 
     target_path = get_model_path(model_type)
     if not target_path:
-        return f"暂不支持这种类型：{model_type}"
+        return f"Jenis ini tidak didukung saat ini：{model_type}"
 
     if isinstance(image_arr, np.ndarray) and image_arr.any() is not None:
         image_filename = filename.rsplit(".", 1)[0] + ".jpeg"
@@ -127,7 +127,7 @@ def download(model_type, filename, url, image_arr):
 
     target_file = os.path.join(target_path, filename)
     if os.path.exists(target_file):
-        return f"已经存在了，不重复下载：\n{target_file}"
+        return f"Sudah ada, tidak ada unduhan duplikat：\n{target_file}"
 
 
     cmd = f'curl -o "{target_file}" "{url}" 2>&1'
@@ -143,9 +143,9 @@ def download(model_type, filename, url, image_arr):
     )
     status_output = ""
     if result.returncode == 0:
-        status_output = f"下载成功，保存到：\n{target_file}\n{result.stdout}"
+        status_output = f"Berhasil mengunduh, simpan ke：\n{target_file}\n{result.stdout}"
     else:
-        status_output = f"下载失败了，错误信息：\n{result.stdout}"
+        status_output = f"Pengunduhan gagal. Pesan kesalahan.：\n{result.stdout}"
 
     return status_output
 
@@ -176,34 +176,34 @@ def on_ui_tabs():
         with gr.Row() as input_component:
             with gr.Column():
                 inp_url = gr.Textbox(
-                    label="Civitai 模型的页面地址，不是下载链接",
-                    placeholder="类似 https://civitai.com/models/28687/pen-sketch-style"
+                    label="Alamat halaman model Civitai, bukan tautan unduhan.",
+                    placeholder="sebanding https://civitai.com/models/28687/pen-sketch-style"
                 )
                 with gr.Row():
-                    preview_btn = gr.Button("预览")
-                    download_btn = gr.Button("下载", interactive=False)
+                    preview_btn = gr.Button("pratinjau")
+                    download_btn = gr.Button("mengunduh", interactive=False)
                 with gr.Row():
                     result = gr.Textbox(
                         # value=result_update,
-                        label="执行结果",
+                        label="Hasil implementasi",
                         interactive=False,
                         # every=1,
                     )
             with gr.Column() as preview_component:
                 with gr.Row():
                     with gr.Column() as model_info_component:
-                        name = gr.Textbox(label="名称", interactive=False)
-                        model_type = gr.Textbox(label="类型", interactive=False)
-                        trained_words = gr.Textbox(label="触发词", interactive=False)
-                        creator = gr.Textbox(label="作者", interactive=False)
-                        tags = gr.Textbox(label="标签", interactive=False)
-                        updated_at = gr.Textbox(label="最近更新时间", interactive=False)
+                        name = gr.Textbox(label="nama", interactive=False)
+                        model_type = gr.Textbox(label="tipologi", interactive=False)
+                        trained_words = gr.Textbox(label="kata pemicu", interactive=False)
+                        creator = gr.Textbox(label="penulis", interactive=False)
+                        tags = gr.Textbox(label="tab (dari jendela) (komputasi)", interactive=False)
+                        updated_at = gr.Textbox(label="Terakhir diperbarui", interactive=False)
                     with gr.Column() as model_image_component:
                         image = gr.Image(
                             show_label=False,
                             interactive=False,
                         )
-                with gr.Accordion("介绍", open=False):
+                with gr.Accordion("hadiah (sb untuk pekerjaan, dll.)", open=False):
                     description = gr.HTML()
         with gr.Row(visible=False):
             filename = gr.Textbox(
@@ -217,8 +217,8 @@ def on_ui_tabs():
                 interactive=False,
             )
         with gr.Row():
-            gr.Markdown(f"版本：{VERSION}\n\n作者：@tzwm\n{footer}")
-
+            gr.Markdown(f"rilis：{VERSION}\n\npenulis：@tzwm\n{footer}")
+            gr.Markdown(f"Modifer：@vorstcavry\n{footer}")
 
         def preview_components():
             return [
@@ -250,6 +250,6 @@ def on_ui_tabs():
             outputs=[result]
         )
 
-    return [(ui_component, "模型下载", "model_downloader_cn_tab")]
+    return [(ui_component, "Unduh Model", "model_downloader_cn_tab")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
